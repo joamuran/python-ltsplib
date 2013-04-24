01f607bb5626496c7626f17be825d6707a2035d9
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2013-04-24 09:06:27 +0200

    Added ltsplib.py skeleton

---
+++ src/ltsplib.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import glob
+import os
+import string
+
+
+class ltspTest:
+	
+	def __init__(self):
+		'''
+		Simple init method
+		'''
+		pass
+	
+
+	def test_chroot(self, chroot_dir):
+		'''
+		test_chroot test if the given directory is a real chroot or 
+		besides, it seems like a chroot.
+		'''
+		if os.path.isdir(chroot_dir):
+			return {'status': True, 'msg':'[N4dChroot] Directory existent'}
+		else:
+			return {'status': False, 'msg':'[N4dChroot] Directory not existent'}
+
+	#def test_chroot(self, chroot_dir)
+	
+#class ltspTest
+
